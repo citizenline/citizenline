30c5ef762cb12344bd4a5022a5798472d5a54ffd
Gerhard Messelink <gerhard.messelink@gmail.com>
2016-03-21 08:08:11 +0100

    CL002: Fix IP address logging

--- citizenline/settings/base.py
+++ citizenline/settings/base.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = (
 #    'bettertexts.middleware.MultiSiteMiddleware',
 
 MIDDLEWARE_CLASSES = (
-    'bettertexts.middleware.SetRemoteAddrFromForwardedFor',
+    'citizenline.middleware.SetRemoteAddrFromForwardedFor',
     'multisite.middleware.CookieDomainMiddleware',
     'multisite.middleware.DynamicSiteMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
